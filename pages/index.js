e0a10bffbd2a68ce1a6ad1239837eaa376ac5844
Lucas Maiorini <lucasmaiorini@gmail.com>
2025-12-18 04:40:44 +0000

    alteração para testar robô deploy

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Alterações para curso.dev</h1>
+    return <h1>Alterações para curso.dev - Novas alterações para checar o robô de Deploy</h1>
 }
 
 export default Home;
