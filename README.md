aaf63ebf0f286d8a0c9bcd73d875a70a3f41662a
Lucas Maiorini <lucasmaiorini@gmail.com>
2026-03-03 03:39:33 +0000

    add scripts `lint:check` & `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 A project copied to simulate a real project used in a programming course. Just for educational purposes :)
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Alterações para curso.dev - Novas alterações para checar o robô de Deploy</h1>
+function Home() {
+  return (
+    <h1>
+      Alterações para curso.dev - Novas alterações para checar o robô de Deploy
+    </h1>
+  );
 }
 
 export default Home;
